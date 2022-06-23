3051fb2ac71dc2e418e8054375844f0a2dfa7420
melodyn <samelodyn@gmail.com>
2022-06-23 21:52:36 +0300

    improves

--- scripts/validity.js
+++ scripts/validity.js
@@ -24,6 +24,8 @@ buttonClosePopupPlace.addEventListener('click', () => closePopup(popupPlace));
 
 /* forms */
 const formPlace = document.forms.place;
+const formPlaceFields = Array.from(formPlace.querySelectorAll('.form__item'));
+const buttonSubmitFormPlace = formPlace.querySelector('.form__submit');
 
 const openPopupPlace = () => {
   formPlace.reset();
@@ -43,10 +45,10 @@ const toggleFormSubmit = (elementSubmit, { disable }) => {
     elementSubmit.setAttribute('disabled', 'disabled');
   }
 };
-const checkFormValidity = (elementForm, elementSubmit) => {
+const checkFormValidity = (elementsFields, elementSubmit) => {
+  console.log(elementsFields);
   toggleFormSubmit(elementSubmit, { disable: true });
-  const fields = Array.from(elementForm.elements);
-  const formIsValid = fields.every(({ validity }) => validity.valid);
+  const formIsValid = elementsFields.every(({ validity }) => validity.valid);
   if (!formIsValid) {
     toggleFormSubmit(elementSubmit, { disable: false });
   }
@@ -72,38 +74,80 @@ const checkFieldValidity = (elementField, elementError, invalidFieldClass) => {
 };
 
 /* enable validation */
-const formPlaceFields = Array.from(formPlace.querySelectorAll('.form__item'));
-const buttonSubmitFormPlace = formPlace.querySelector('.form__submit');
+// const formPlaceFields = Array.from(formPlace.querySelectorAll('.form__item'));
+// const buttonSubmitFormPlace = formPlace.querySelector('.form__submit');
+//
+// formPlaceFields.forEach((elementField) => {
+//   const errorTextContainerSelector = `.form__item-error_field_${elementField.name}`;
+//   const elementError = formPlace.querySelector(errorTextContainerSelector);
+//
+//   elementField.addEventListener('input', (e) => {
+//     const field = e.target; // поле взято из события просто для примера, что можно и так
+//     checkFormValidity(formPlaceFields, buttonSubmitFormPlace);
+//     checkFieldValidity(field, elementError, 'form__item_invalid');
+//   });
+//
+//   elementField.addEventListener('focus', focusHandler);
+// });
+//
+// const commonFormHandler = (e) => {
+//   e.preventDefault();
+//   const formIsValid = checkFormValidity(formPlaceFields, buttonSubmitFormPlace);
+//   if (!formIsValid) { // прервать выполнение кода ниже и остановить событие
+//     e.stopImmediatePropagation();
+//   }
+// };
+//
+// const submitPlaceHandler = (e) => {
+//   const name = e.target.name.value;
+//   const link = e.target.link.value;
+//
+//   closePopup(popupPlace);
+//
+//   const place = createPlace(name, link);
+//   addPlace(place);
+// };
 
 formPlaceFields.forEach((elementField) => {
   const errorTextContainerSelector = `.form__item-error_field_${elementField.name}`;
   const elementError = formPlace.querySelector(errorTextContainerSelector);
 
   elementField.addEventListener('input', (e) => {
-    const field = e.target; // поле взято из события просто для примера, что можно и так
-    checkFormValidity(formPlace, buttonSubmitFormPlace);
-    checkFieldValidity(field, elementError, 'form__item_invalid');
-  });
+    const fieldIsValid = elementField.validity.valid;
+    elementError.textContent = elementField.validationMessage;
+    if (!fieldIsValid) {
+      elementField.classList.add('form__item_invalid');
+    } else {
+      elementField.classList.remove('form__item_invalid');
+    }
+    // можно использовать event, если хочется:
+    // const fieldIsValid = e.target.validity.valid;
+    // elementError.textContent = e.target.validationMessage;
+    // ...и так далее
 
-  elementField.addEventListener('focus', focusHandler);
+    const formIsValid = formPlaceFields.every(({ validity }) => validity.valid);
+    if (formIsValid) {
+      buttonSubmitFormPlace.removeAttribute('disabled');
+    }
+  });
 });
 
 const submitPlaceHandler = (e) => {
   e.preventDefault();
-  const formIsValid = checkFormValidity(formPlace, buttonSubmitFormPlace);
-  if (!formIsValid) { // прервать выполнение кода ниже и остановить событие
-    e.stopImmediatePropagation();
-    return;
+  buttonSubmitFormPlace.setAttribute('disabled', 'disabled');
+
+  const formIsValid = formPlaceFields.every(({ validity }) => validity.valid);
+  if (formIsValid) {
+    const name = e.target.name.value;
+    const link = e.target.link.value;
+
+    closePopup(popupPlace);
+
+    const place = createPlace(name, link);
+    addPlace(place);
   }
-
-  const name = e.target.name.value;
-  const link = e.target.link.value;
-
-  closePopup(popupPlace);
-
-  const place = createPlace(name, link);
-  addPlace(place);
 };
 
+// formPlace.addEventListener('submit', commonFormHandler);
 formPlace.addEventListener('submit', submitPlaceHandler);
 dataContainer.addEventListener('focus', openPopupPlace);
